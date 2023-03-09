54e750d7b1455689709c77b20b6b5728e727f6ec
Eisen Berg <1523375@claurendeau.qc.ca>
2023-03-09 16:28:02 -0500

    Ajout des méthodes ajoutant des éléments : void ajoute(), boolean ajoute(), void ajoute(Vecteur x)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        Vecteurs monVecteur = new Vecteurs();
+        monVecteur.ajoute("Allo",0);
+        monVecteur.ajoute("Comment", 1);
+        monVecteur.ajoute("Ca", 2);
+        monVecteur.ajoute("Va", 3);
+        Vecteurs monVecteur1 = new Vecteurs();
+        monVecteur1.ajoute("Hey",0);
+        monVecteur1.ajoute("Salur", 1);
+        monVecteur1.ajoute("1", 2);
+        monVecteur1.ajoute("2", 3);
+        monVecteur.ajoute(monVecteur1);
+        monVecteur.imprimer();
     }
 }
--- src/Vecteurs.java
+++ src/Vecteurs.java
@@ -8,4 +8,42 @@ public class Vecteurs {
         this.mTableau = new String[TAILLE_INITIALE];
         this.nbElements = mTableau.length - 1;
     }
+    public void ajoute(String pElement){
+        mTableau[mTableau.length - 1] = pElement;
+    }
+    public boolean ajoute(String pElement, int index){
+        if (index >= mTableau.length) {
+            return false;
+        }
+        else {
+            mTableau[index] = pElement;
+            return true;
+        }
+    }
+    public String getElementAt(int index) {
+        return mTableau[index];
+    }
+    public int getLength(){
+        return this.mTableau.length;
+    }
+    public void ajoute(Vecteurs pCollection) {
+        String[] newTableau = new String[pCollection.getLength() + this.mTableau.length];
+        for (int i = 0 ; i < this.mTableau.length ; i++){
+            newTableau[i] = this.mTableau[i];
+        }
+        for(int i = 0 ; i < pCollection.getLength(); i++){
+            newTableau[mTableau.length + i] = pCollection.getElementAt(i);
+        }
+        this.mTableau = newTableau;
+        }
+    public void imprimer(){
+        System.out.print("[ ");
+        for(int i = 0; i < mTableau.length; i++) {
+            System.out.printf("\"%s\"", mTableau[i]);
+            if (i != mTableau.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ]");
+    }
 }
